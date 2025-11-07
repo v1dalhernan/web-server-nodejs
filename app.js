aa8510c31ede5e06a08d1d0eeb09c38e87bf407a
Jhonathan Vidal <jhonathanvidalvv@gmail.com>
2025-11-07 16:16:13 -0500

    cambios inicales

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const express = require('express');
+
+const app = express();
 
 
-console.log('Hola mudo');
+
+// midelware para servir contenido estatico 
+app.use(express.static('public'));
+
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+});
+
+// para todos aquellos que no este en las rutas 
+// app.use((req, res) => {
+//   res.sendFile(__dirname +'/public/404.html');
+// });
+
+app.listen(8080);
